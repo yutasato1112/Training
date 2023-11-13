4d468330a4d92c6ec5d5df4c4d65343f9985337e
digpac0522 <banquet.sonic_0l@icloud.com>
2023-11-14 03:25:57 +0900

    add

--- Test/ss.py
+++ Test/ss.py
@@ -1,4 +1,13 @@
-lists=[None]
-for j in range(5):
-    j=str(j)
-    print(lists[j])
+als=[[100,89,93,72,19],[67,78,45,89,23],[24,87,89,17,49],[45,72,83,16,94],[87,37,94,27,84]]
+bls=["A","B","C","D","E"]
+for i in range(len(als)):
+    suma=sum(als[i])
+    name=bls[i]
+    print(str(name)+"'s sum score :"+str(suma))
+        
+
+
+
+
+# print(str(j)+"'s sum score :"+str(suma))
+
--- Test/test5.py
+++ Test/test5.py
@@ -1,8 +1,43 @@
-A=[100,89,93,72,19]
-B=[67,78,45,89,23]
-C=[24,87,89,17,49]
-D=[45,72,83,16,94]
-E=[87,37,94,27,84]
-
-for i in range(A,E):
+a=[[100,89,93,72,19],[67,78,45,89,23],[24,87,89,17,49],[45,72,83,16,94],[87,37,94,27,84]]
+jap=[]
+mat=[]
+sce=[]
+soc=[]
+eng=[]
+for i in a:
+    jap.append(i[0])
+    mat.append(i[1])
+    sce.append(i[2])
+    soc.append(i[3])
+    eng.append(i[4])
+jap.sort()
+mat.sort()
+sce.sort()
+soc.sort()
+eng.sort()
+js=sum(jap)
+ms=sum(mat)
+ss1=sum(sce)
+ss2=sum(soc)
+es=sum(eng)
+jap_t=js/(len(jap))
+mat_t=ms/(len(mat))
+sce_t=ss1/(len(sce))
+soc_t=ss2/(len(soc))
+eng_t=es/(len(eng))
+jc=jap[2]
+mc=mat[2]
+sc1=sce[2]
+sc2=soc[2]
+ec=eng[2]
+print("Avaragesocore of Japnanese :"+str(jap_t)+" median score :"+str(jc))
+print("Avaragesocore of Math :"+str(mat_t)+" median score :"+str(mc))
+print("Avaragesocore of Scence :"+str(sce_t)+" median score :"+str(sc1))
+print("Avaragesocore of Social Study :"+str(soc_t)+" median score :"+str(sc2))
+print("Avaragesocore of English :"+str(eng_t)+" median score :"+str(ec))
+bls=["A","B","C","D","E"]
+for i in range(len(a)):
+    suma=sum(a[i])
+    name=bls[i]
+    print(str(name)+"'s sum score :"+str(suma))
     
